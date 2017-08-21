844d7f16cd9c3f11631f82c93fa1b35bd69688ef
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-08-21 15:14:07 +0200

    boost bugfix

--- share/modules/FindMyBoost.cmake
+++ share/modules/FindMyBoost.cmake
@@ -3,12 +3,12 @@
 #
 # Output:
 #    Boost_FOUND
-#    Boost_INCLUDE_DIR
+#    Boost_INCLUDE_DIRS
 #
 #############################################################################
 
 IF(IS_DIRECTORY "${DEVELOPMENT_ROOT}/thirdparty/boost/1.59.0/boost")
-    SET(Boost_INCLUDE_DIR "${DEVELOPMENT_ROOT}/thirdparty/boost/1.59.0")
+    SET(Boost_INCLUDE_DIRS "${DEVELOPMENT_ROOT}/thirdparty/boost/1.59.0")
 ELSE(IS_DIRECTORY "${DEVELOPMENT_ROOT}/thirdparty/boost/1.59.0/boost")
     FIND_PACKAGE(Boost REQUIRED)
 ENDIF(IS_DIRECTORY "${DEVELOPMENT_ROOT}/thirdparty/boost/1.59.0/boost")
